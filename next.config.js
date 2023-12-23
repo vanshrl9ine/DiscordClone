0f0471c4d44c516da8281c95f30de45509786b95
Vansh <gu[ptavansh67@gmail.com>
2023-12-24 02:58:04 +0530

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images:{
+        domains:[
+            "uploadthing.com"
+        ]
+    }
+}
+
+module.exports = nextConfig
+module.exports = {
+    images: {
+      domains: ['utfs.io'],
+    },
+  }
+  
